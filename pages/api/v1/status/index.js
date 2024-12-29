f4b8b0005ac4bd72fdeb69e6134cf42797f2dcf1
Irailton Reis <18430248+irailtonreis@users.noreply.github.com>
2024-12-29 01:48:15 +0000

    add new props and tests endpoint status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,30 @@
 import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseMaxConnnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnnectionsValue =
+    databaseMaxConnnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity where datname = 'local_db';",
+  );
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
   response.status(200).json({
     updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
   });
 }
 
